2db47643a80555fe8137e9a95604913452df18e2
Fadlan <55142169+fadlan98@users.noreply.github.com>
2023-04-28 15:55:53 +0700

    Update ResScrapTA.py

--- ResScrapTA.py
+++ ResScrapTA.py
@@ -2,70 +2,6 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup as soup 
 
-NO_OF_CHARS = 256
- 
-def badCharHeuristic(string, size):
-    '''
-    The preprocessing function for
-    Boyer Moore's bad character heuristic
-    '''
- 
-    # Initialize all occurrence as -1
-    badChar = [-1]*NO_OF_CHARS
- 
-    # Fill the actual value of last occurrence
-    for i in range(size):
-        badChar[ord(string[i])] = i;
- 
-    # retun initialized list
-    return badChar
- 
-def search(txt, pat):
-    '''
-    A pattern searching function that uses Bad Character
-    Heuristic of Boyer Moore Algorithm
-    '''
-    m = len(pat)
-    n = len(txt)
- 
-    # create the bad character list by calling
-    # the preprocessing function badCharHeuristic()
-    # for given pattern
-    badChar = badCharHeuristic(pat, m)
- 
-    # s is shift of the pattern with respect to text
-    s = 0
-    while(s <= n-m):
-        j = m-1
- 
-        # Keep reducing index j of pattern while
-        # characters of pattern and text are matching
-        # at this shift s
-        while j>=0 and pat[j] == txt[s+j]:
-            j -= 1
- 
-        # If the pattern is present at current shift,
-        # then index j will become -1 after the above loop
-        if j<0:
-            '''   
-                Shift the pattern so that the next character in text
-                      aligns with the last occurrence of it in pattern.
-                The condition s+m < n is necessary for the case when
-                   pattern occurs at the end of text
-               '''
-            s += (m-badChar[ord(txt[s+m])] if s+m<n else 1)
-            return s
-        else:
-            '''
-               Shift the pattern so that the bad character in text
-               aligns with the last occurrence of it in pattern. The
-               max function is used to make sure that we get a positive
-               shift. We may get a negative shift if the last occurrence
-               of bad character in pattern is on the right side of the
-               current character.
-            '''
-            s += max(1, j-badChar[ord(txt[s+j])])
-    return -1
 
 def main():
     headers = {
@@ -79,28 +15,9 @@ def main():
     ratings = []
     pageOffset = []
 
-    #get max page
-    # size = 30
-    # url = 'https://www.tripadvisor.com/RestaurantSearch?Action=PAGE&ajax=1&availSearchEnabled=false&sortOrder=popularity&geo=297725&itags=10591&o=a'+ str(size)
-    # r = requests.get(url)
-    # soup1 = soup(r.text, "html.parser")
-    # linkw = soup1.find_all('a')[-1]
-    # page_number = linkw.get('data-page-number')
-    # maxPage = int(page_number) # *30
-    
-
-    # for pn in range(int(maxPage)):
-    #     pageOffset.append((pn) * 30)
-
-    #print(pageOffset)
-    # #get all the links from the page
-    
-    #for offset in (pageOffset): ##This one's for get all the page
+  
     for offset in range(0,610,30):  
-        # if offset == 0:
-        #     print('processing page 1...')
-        #     html = requests.get('https://www.tripadvisor.com/Restaurants-g297725-Medan_North_Sumatra_Sumatra.html', headers = headers)
-        # elif offset > 0:
+
         print('processing page '+ str(int((offset/30))+1)+'...')
         html = requests.get('https://www.tripadvisor.com/RestaurantSearch-g297725-oa'+ str(offset)+'-Medan_North_Sumatra_Sumatra.html#EATERY_LIST_CONTENTS', headers = headers)
         bsobj = soup(html.content,'html.parser')
@@ -124,8 +41,6 @@ def main():
         ratsfound = 0
         d = [5,10,15,20,25]
         html2 = requests.get(nlink.format(i for i in d),headers=headers)
-            #sleep(randint(1,5))
-
         bsobj2 = soup(html2.content,'html.parser')
         print('processing name in ' + str(nlink) +" link...")
         for r in bsobj2.findAll('h1',{'class':'_3a1XQ88S'}): 
@@ -186,16 +101,6 @@ def main():
     writer = pd.ExcelWriter('allRestaurant.xlsx', engine='xlsxwriter')
     restaurantDF.to_excel(writer, sheet_name='Sheet1',index=False)
     writer.save()
-
-        
-    #print("Restaurant Found")
-#print(len(pNumb),len(name),len(location),len(ratings))
-    # restaurantDF = pd.DataFrame({
-    # 'Nama':name,
-    # 'Alamat':location,
-    # 'No. Telp':pNumb,
-    # 'Rating':ratings})
-    # restaurantDF.to_csv('restaurantcth2.csv',index=False)
 
 if __name__ == '__main__':
     main()
